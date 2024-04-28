0fa6407de15601491836cabc714b21910c608faa
Alimur Razi <138320899+alimurdipu@users.noreply.github.com>
2024-04-28 22:49:15 +0600

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('.slider-container').slick({
+    //   setting-name: setting-value
+    fade: true,
+    arrows: true,
+    prevArrow: ".previous",
+    nextArrow: ".next",
+    autoplay: true,
+    autoplaySpeed: 2000
+    });
+  });
